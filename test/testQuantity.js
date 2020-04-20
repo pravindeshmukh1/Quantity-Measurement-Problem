d0b5a93fa6502933991d2a13d115765ead1f61a9
pravindeshmukh1 <pravin1desh@gmail.com>
2020-04-21 02:19:26 +0530

    ADDED: Cheack not Equality length for 0 Ft and 1 Ft

--- test/testQuantity.js
+++ test/testQuantity.js
@@ -6,4 +6,8 @@ describe('Testing for Length Check', function () {
         let lengthMeasurementCheck = lengthMeasurement.checkLength(0, 0);
         assert.equal(true, lengthMeasurementCheck)
     })
+    it('given value 0 Ft and 1 Ft Length if equal should return not true', function () {
+        let lengthMeasurementCheck = lengthMeasurement.checkLength(0, 1);
+        assert.notEqual(true, lengthMeasurementCheck)
+    })
 })
